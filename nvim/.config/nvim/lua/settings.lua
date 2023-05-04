cf0290291627261bfa4c50c8965b36b884d05e03
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2023-05-04 10:27:31 +0630

    removed blamer.lua and added back to settings.lua

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -48,3 +48,10 @@ vim.cmd([[
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.cmd.colorscheme "catppuccin-mocha"
+
+-- blamer.nvim settings
+vim.g.blamer_enabled = true
+vim.g.blamer_delay = 500
+vim.g.blamer_show_in_visual_modes = 0
+vim.g.blamer_show_in_insert_modes = 0
+vim.g.blamer_prefix = " > "
