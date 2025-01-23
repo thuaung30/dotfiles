91992f29df68ea97e911af7912bf143dd230638d
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2025-01-23 10:45:08 +0630

    chore: added config

--- stowdis.sh
+++ stowdis.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 if [[ -z $STOW_FOLDERS ]]; then
-	STOW_FOLDERS="alacritty,fish,gitconfig,nvim,scripts,skhd,starship,tmux,yabai,zsh"
+	STOW_FOLDERS="alacritty,fish,ghostty,gitconfig,nvim,scripts,skhd,starship,tmux,yabai,zsh"
 fi
 
 if [[ -z $DOTFILES ]]; then
