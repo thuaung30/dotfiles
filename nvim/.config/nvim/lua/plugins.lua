603e3e70d799212c1afd47238364c2e07b13a4e2
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2025-03-15 18:09:29 +0630

    chore: disabled scope and scroll in snacks.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -90,8 +90,6 @@ return {
             picker = { enabled = true },
             notifier = { enabled = true },
             quickfile = { enabled = true },
-            scope = { enabled = true },
-            scroll = { enabled = true },
             statuscolumn = { enabled = true },
             words = { enabled = true },
         },
