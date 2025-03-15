aa5942192c7c65fe512d8b6847fefc5b059a2d08
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2025-03-15 16:45:38 +0630

    ops: installed mini.nvim and snacks.nvim

--- nvim/.config/nvim/after/plugin/colors.lua
+++
@@ -1,6 +0,0 @@
-function ColorVim(color)
-    color = color or "base16-horizon-dark"
-    vim.cmd.colorscheme(color)
-end
-
--- ColorVim()
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,6 +1,9 @@
 return {
     -- telescope
-    { "nvim-lua/plenary.nvim",            lazy = true },
+    {
+        "nvim-lua/plenary.nvim",
+        lazy = true
+    },
     {
         "nvim-telescope/telescope.nvim",
         lazy = true,
@@ -12,42 +15,127 @@ return {
             { "<C-p>",      "<cmd>lua require('telescope.builtin').git_files()<cr>" }
         }
     },
-    { "chriskempson/base16-vim",          lazy = true },
-    { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" },
-    -- LSP Support
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    -- LSP Zero
-    { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
+    -- telescope
     {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+        "chriskempson/base16-vim",
+        lazy = true
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         lazy = true,
         config = function()
-            local cmp = require('cmp')
-
-            cmp.setup({
-                sources = {
-                    { name = "nvim_lsp" },
+            require 'nvim-treesitter.configs'.setup {
+                -- A list of parser names, or "all" (the four listed parsers should always be installed)
+                ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "vim" },
+                ignore_install = {},
+                modules = {},
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+                highlight = {
+                    -- `false` will disable the whole extension
+                    enable = true,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
                 },
-                mapping = {
-                    ['<C-p>'] = cmp.mapping.select_prev_item({ behaviour = "select" }),
-                    ['<C-n>'] = cmp.mapping.select_next_item({ behaviour = "select" }),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = false }),
-                    ['<Tab>'] = cmp.mapping.confirm({ select = false }),
-                    ['<S-Tab>'] = nil
-                }
-            })
+            }
         end
     },
-    { "L3MON4D3/LuaSnip",             lazy = true },
-    { "tpope/vim-commentary" },
-    { "tpope/vim-fugitive",           cmd = "G",    lazy = true },
-    { "tpope/vim-surround" },
-    { "kyazdani42/nvim-web-devicons", lazy = true },
-    { "nvim-lualine/lualine.nvim" },
-    { "catppuccin/nvim",              lazy = false, name = "catppuccin" },
-    { 'rose-pine/neovim',             lazy = true,  name = 'rose-pine' }
+    -- LSP Support
+    {
+        "williamboman/mason.nvim",
+        lazy = true
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = true
+    },
+    -- LSP Zero
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x"
+    },
+    { "neovim/nvim-lspconfig" },
+    -- mini.nvim, qol plugin
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        config = function()
+            -- text editing
+            require("mini.comment").setup()
+            require("mini.surround").setup()
+            require("mini.pairs").setup()
+            require("mini.completion").setup()
+            require("mini.snippets").setup()
+            require("mini.splitjoin").setup()
+            -- text editing
+            -- general workflow
+            require("mini.bracketed").setup()
+            require("mini.git").setup()
+            require("mini.diff").setup()
+            -- general workflow
+            -- appearance
+            require("mini.icons").setup()
+            require("mini.statusline").setup()
+            -- appearance
+        end
+    },
+    -- mini.nvim, qol plugin
+    -- snacks.nvim qol plugin
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            bigfile = { enabled = true },
+            dashboard = { enabled = true },
+            indent = { enabled = true },
+            input = { enabled = true },
+            picker = { enabled = true },
+            notifier = { enabled = true },
+            quickfile = { enabled = true },
+            scope = { enabled = true },
+            scroll = { enabled = true },
+            statuscolumn = { enabled = true },
+            words = { enabled = true },
+        },
+    },
+    -- snacks.nvim qol plugin
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin"
+    },
+    {
+        'rose-pine/neovim',
+        lazy = true,
+        name = 'rose-pine'
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("cyberdream").setup({
+                transparent = true,
+                italic_comments = true,
+                hide_fillchars = true,
+                terminal_colors = true,
+                cache = true,
+                borderless_telescope = true
+            })
+        end,
+    }
 }
