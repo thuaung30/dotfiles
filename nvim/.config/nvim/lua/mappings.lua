b0d13d8adf2376c0712289646e13e81bb232727a
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2025-03-31 09:12:42 +0630

    feat: installed conform.nvim

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -59,7 +59,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- disabled this keymap since we are using conform to format
+-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -47,6 +47,46 @@ return {
         branch = "v3.x"
     },
     { "neovim/nvim-lspconfig" },
+    -- LSP Zero
+    -- Formatter
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        keys = {
+            {
+                "<leader>f",
+                function()
+                    require("conform").format({ async = true })
+                end,
+                mode = "",
+                desc = "Format buffer",
+            },
+        },
+        -- This will provide type hinting with LuaLS
+        ---@module "conform"
+        ---@type conform.setupOpts
+        opts = {
+            -- Define your formatters
+            formatters_by_ft = {
+                python = { "yapf" },
+            },
+            -- Set default options
+            default_format_opts = {
+                lsp_format = "fallback",
+            },
+            -- Set up format-on-save
+            format_on_save = { timeout_ms = 999 },
+            -- Customize formatters
+            formatters = {
+            },
+        },
+        init = function()
+            -- If you want the formatexpr, here is the place to set it
+            vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+        end,
+    },
+    -- Formatter
     -- mini.nvim, qol plugin
     {
         "echasnovski/mini.nvim",
